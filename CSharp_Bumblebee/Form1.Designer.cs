3033355bd59f2f3751ca4a0844a1a9a50a1f9c7c
boubou417 <51277941+boubou417@users.noreply.github.com>
2026-08-07 15:23:56 +0800

    Separate logo from centered controls in responsive header

--- CSharp_Bumblebee/Form1.Designer.cs
+++ CSharp_Bumblebee/Form1.Designer.cs
@@ -17,13 +17,11 @@ namespace CSharp_Bumblebee
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.headerPanel = new System.Windows.Forms.Panel();
-            this.headerLayout = new System.Windows.Forms.TableLayoutPanel();
-            this.cbDisparity = new System.Windows.Forms.CheckBox();
             this.pBoxLogo = new System.Windows.Forms.PictureBox();
+            this.cbDisparity = new System.Windows.Forms.CheckBox();
             this.imagePanel = new System.Windows.Forms.Panel();
             this.pBox = new System.Windows.Forms.PictureBox();
             this.headerPanel.SuspendLayout();
-            this.headerLayout.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxLogo)).BeginInit();
             this.imagePanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
@@ -33,47 +31,34 @@ namespace CSharp_Bumblebee
             this.backgroundWorker1.WorkerSupportsCancellation = true;
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
 
+            // Header area. The logo is anchored independently on the right so
+            // it cannot overlap the centered Connect / Start buttons.
             this.headerPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                 | System.Windows.Forms.AnchorStyles.Right)));
-            this.headerPanel.Controls.Add(this.headerLayout);
+            this.headerPanel.Controls.Add(this.pBoxLogo);
+            this.headerPanel.Controls.Add(this.cbDisparity);
             this.headerPanel.Location = new System.Drawing.Point(31, 48);
             this.headerPanel.Name = "headerPanel";
             this.headerPanel.Size = new System.Drawing.Size(1400, 112);
             this.headerPanel.TabIndex = 5;
 
-            this.headerLayout.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.headerLayout.ColumnCount = 4;
-            this.headerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.headerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 135F));
-            this.headerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 135F));
-            this.headerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 210F));
-            this.headerLayout.Controls.Add(this.cbDisparity, 0, 0);
-            this.headerLayout.Controls.Add(this.pBoxLogo, 3, 0);
-            this.headerLayout.Location = new System.Drawing.Point(385, 0);
-            this.headerLayout.Name = "headerLayout";
-            this.headerLayout.RowCount = 1;
-            this.headerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.headerLayout.Size = new System.Drawing.Size(630, 108);
-            this.headerLayout.TabIndex = 0;
-
-            this.cbDisparity.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbDisparity.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.cbDisparity.AutoSize = true;
             this.cbDisparity.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.cbDisparity.FlatAppearance.BorderColor = System.Drawing.Color.DarkBlue;
             this.cbDisparity.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbDisparity.Font = new System.Drawing.Font("中國龍特圓體", 14F);
             this.cbDisparity.ForeColor = System.Drawing.Color.DarkGoldenrod;
-            this.cbDisparity.Location = new System.Drawing.Point(27, 42);
+            this.cbDisparity.Location = new System.Drawing.Point(330, 43);
             this.cbDisparity.Name = "cbDisparity";
             this.cbDisparity.Size = new System.Drawing.Size(96, 23);
             this.cbDisparity.TabIndex = 2;
             this.cbDisparity.Text = "Disparity";
             this.cbDisparity.UseVisualStyleBackColor = false;
 
-            this.pBoxLogo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pBoxLogo.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.pBoxLogo.InitialImage = null;
-            this.pBoxLogo.Location = new System.Drawing.Point(430, 4);
-            this.pBoxLogo.Margin = new System.Windows.Forms.Padding(10, 4, 4, 4);
+            this.pBoxLogo.Location = new System.Drawing.Point(1040, 4);
             this.pBoxLogo.Name = "pBoxLogo";
             this.pBoxLogo.Size = new System.Drawing.Size(190, 100);
             this.pBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -107,8 +92,7 @@ namespace CSharp_Bumblebee
             this.Name = "Form1";
             this.Text = "Form1";
             this.headerPanel.ResumeLayout(false);
-            this.headerLayout.ResumeLayout(false);
-            this.headerLayout.PerformLayout();
+            this.headerPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxLogo)).EndInit();
             this.imagePanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
@@ -119,7 +103,6 @@ namespace CSharp_Bumblebee
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Panel headerPanel;
-        private System.Windows.Forms.TableLayoutPanel headerLayout;
         private System.Windows.Forms.Panel imagePanel;
         private System.Windows.Forms.PictureBox pBox;
         private System.Windows.Forms.CheckBox cbDisparity;
