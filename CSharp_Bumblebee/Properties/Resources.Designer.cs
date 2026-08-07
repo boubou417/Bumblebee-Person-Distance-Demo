37091395bdd33398adb5dcf3a8b1e79daf6f00d6
boubou417 <51277941+boubou417@users.noreply.github.com>
2026-08-07 14:21:14 +0800

    Add baseline resources designer

---
+++ CSharp_Bumblebee/Properties/Resources.Designer.cs
@@ -0,0 +1,19 @@
+namespace CSharp_Bumblebee.Properties
+{
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class Resources
+    {
+        private static global::System.Resources.ResourceManager resourceMan;
+        private static global::System.Globalization.CultureInfo resourceCulture;
+        internal Resources() { }
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get { if ((resourceMan == null)) resourceMan = new global::System.Resources.ResourceManager("CSharp_Bumblebee.Properties.Resources", typeof(Resources).Assembly); return resourceMan; }
+        }
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture { get { return resourceCulture; } set { resourceCulture = value; } }
+    }
+}
